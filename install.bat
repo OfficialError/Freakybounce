acf28ac01a4add5f085776170b8416689b9d894a
Error <45919406+OfficialError@users.noreply.github.com>
2024-08-17 00:07:02 +0200

    Delete install.bat

--- install.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Copying built files to liquidlauncher installation.
-xcopy /E /Y dist %appdata%\CCBlueX\LiquidLauncher\data\gameDir\nextgen\LiquidBounce\themes\femboy > nul
-if %errorlevel% neq 0 (
-    echo An error occured while copying the files: %errorlevel%
-) else (
-    echo Copied the files successfully.
-)
-@echo on
